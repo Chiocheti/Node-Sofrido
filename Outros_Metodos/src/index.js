0add4399ed712c4205db985400e7678a7792cdb9
Chiocheti <gabrielchiocheti@gmail.com>
2021-09-09 23:27:26 -0300

    Inicio Dessa Merda

---
+++ Outros_Metodos/src/index.js
@@ -0,0 +1,59 @@
+// Importar o express
+const { response, request } = require("express");
+const express = require ("express");
+// Criando a instancia (objeto)
+const App = express();
+
+App.get('/Projetos' , (request, response) => {
+
+    return response.json([
+        "Projeto1",
+        "Projeto2",
+        "Projeto3"
+    ]);
+
+});
+
+App.post('/Projetos2' , (request,response) => {
+
+    return response.json([
+
+        "Projeto 1",
+        "Projeto 2",
+        "Projeto 3",
+        "Projeto 4"
+    ]);
+
+})
+
+App.put('/Projeto3' , (request,response) => {
+    
+    return response.json([
+
+        "Projeto 5",
+        "Projeto 6",
+        "Projeto 7",
+        "Projeto 8"
+
+    ]);
+})
+
+App.delete('/Projetos2' , (request,response) => {
+
+    return response.json([
+
+        "Projeto 1",
+        "Projeto 2",
+        "Projeto 3",
+
+    ]);
+
+})
+
+App.listen(3333 , () => {
+
+    console.log("+----------------+");
+    console.log("| PAI TA ON !!!! |");
+    console.log("+----------------+");
+
+});
